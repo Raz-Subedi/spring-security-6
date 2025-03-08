47d740a60ff3f846cb3cab64ac796bc30af5ba68
Raz Subedi <subedirazkumar@gmail.com>
2025-03-08 21:55:11 +0545

    Initial Commit

---
+++ src/main/java/com/dailycodebuffer/security/entity/User.java
@@ -0,0 +1,39 @@
+package com.dailycodebuffer.security.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private Integer id;
+    private String username;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
